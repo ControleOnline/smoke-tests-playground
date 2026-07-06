b08ee5d79aa302fe5ea8ee42f12173599757bc12
Luiz Kim <luiz.kim@controleonline.com>
2026-07-06 12:38:35 -0300

    implementa a interface PrependExtensionInterface e adiciona método prepend para configuração do Twig

--- src/DependencyInjection/SmokeTestsPlaygroundExtension.php
+++ src/DependencyInjection/SmokeTestsPlaygroundExtension.php
@@ -7,10 +7,20 @@ namespace ControleOnline\SmokeTestsPlayground\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class SmokeTestsPlaygroundExtension extends Extension
+final class SmokeTestsPlaygroundExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('twig', [
+            'paths' => [
+                dirname(__DIR__, 2).'/templates' => 'SmokeTestsPlayground',
+            ],
+        ]);
+    }
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__, 2).'/config'));
